e163bdd7a62b127a8c8218deca1c28e5586c0e10
cristian0497 <cristian04.97@hotmail.com>
2019-09-18 20:22:31 +0000

    Size Var

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-  char _character;
-  int _integer;
-  long _long;
-  long long _longlong;
-  float _float;
+char _character;
+int _integer;
+long _long;
+long long _longlong;
+float _float;
 
-  printf("Size of a char: %lu byte(s)\n", sizeof(_character));
-  printf("Size of an int: %lu byte(s)\n", sizeof(_integer));
-  printf("Size of a long int: %lu byte(s)\n", sizeof(_long));
-  printf("Size of a long long int: %lu byte(s)\n", sizeof(_longlong));
-  printf("Size of a float: %lu byte(s)\n", sizeof(_float));
+printf("Size of a char: %lu byte(s)\n", sizeof(_character));
+printf("Size of an int: %lu byte(s)\n", sizeof(_integer));
+printf("Size of a long int: %lu byte(s)\n", sizeof(_long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(_longlong));
+printf("Size of a float: %lu byte(s)\n", sizeof(_float));
 
-  return (0);
+return (0);
 }
