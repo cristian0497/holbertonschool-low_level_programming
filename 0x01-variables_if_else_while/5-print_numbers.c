0634944ce12e335664cf424d89c920091ae79b37
cristian0497 <cristian04.97@hotmail.com>
2019-09-19 20:48:13 +0000

    Numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - the last digit of the number n
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int count;
+for (count = 0; count < 10; count++)
+printf("%d", count);
+printf("\n");
+return (0);
+}
