fbab99a3138abf5401fa5d14cec258f6d5afd8ed
cristian0497 <cristian04.97@hotmail.com>
2019-09-19 19:40:27 +0000

    Alphabeth in Upper and lowe

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - alphabeth lower and upper
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char character;
+for (character = 'a'; character <= 'z'; character++)
+putchar (character);
+for (character = 'A'; character <= 'Z'; character++)
+putchar (character);
+putchar ('\n');
+return (0);
+}
