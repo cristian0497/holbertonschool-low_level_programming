1078642c933fd62de8a235f50b20827b499b98e1
cristian0497 <cristian04.97@hotmail.com>
2019-09-19 19:18:56 +0000

    Alphabet with For

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - alphabeth
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char character;
+for (character = 'a'; character <= 'z'; character++)
+putchar (character);
+return (0);
+}
