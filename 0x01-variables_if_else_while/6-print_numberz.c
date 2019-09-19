95070f89ad7b9432114520017bb9c0c94b4947c8
cristian0497 <cristian04.97@hotmail.com>
2019-09-19 21:03:09 +0000

    numbers like char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - the last digit of the number n
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char count;
+for (count = '0'; count <= '9'; count++)
+putchar (count);
+putchar('\n');
+return (0);
+}
