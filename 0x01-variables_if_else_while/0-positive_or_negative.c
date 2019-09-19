05f8c814a0c8f5c9bcebe861f088233f7cc619e2
cristian0497 <cristian04.97@hotmail.com>
2019-09-19 17:29:43 +0000

    Correction - Positive,negative or iqual

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Funtion Main search and define the
+ * type of the number positive, negative or zero
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -11,11 +13,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positive\n", n);
+printf("%dis positive\n", n);
 else if (n < 0)
-printf("%d is negative\n", n);
+printf("%dis negative\n", n);
 else if (n == 0)
-printf("%d is zero\n", n);
+printf("%dis zero\n", n);
 
 return (0);
 }
