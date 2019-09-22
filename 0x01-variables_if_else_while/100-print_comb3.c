ba77c89c0a95cdaa8d462f97273f01b593f85575
cristian0497 <cristian04.97@hotmail.com>
2019-09-22 01:02:21 +0000

    Solution

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,34 +6,21 @@
 int main(void)
 {
 	int a, b;
-<<<<<<< HEAD
 	int c = 49;
 
 	for (a = 48; a < 57; a++)
 	{
 		for (b = c; b <= 57; b++)
-	{
-	putchar (a);
-	putchar (b);
-	if (a != 56)
-	{
-		putchar (',');
-		putchar (32);
-	}
-	}
+		{
+			putchar (a);
+			putchar (b);
+			if (a != 56)
+			{
+				putchar (',');
+				putchar (32);
+			}
+		}
 	c++;
-=======
-
-	for (a = 48; a < 57; a++)
-	{
-	for (b = 49; b < 58; b++)
-	{
-	putchar (a);
-	putchar (b);
-	putchar (',');
-	putchar (32);
-	}
->>>>>>> 3b905450020ca06a6f491115eae49f8368c355d7
 	}
 	putchar ('\n');
 return (0);
