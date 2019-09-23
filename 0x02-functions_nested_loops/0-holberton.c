a5d0da655f24f332fa27cdcd9563e85b0e103651
cristian0497 <cristian04.97@hotmail.com>
2019-09-23 21:05:28 +0000

    Arrays with Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *
+ *
+ */
+
+int main ()
+{
+	char letter [9] = "Holberton";
+	int i;
+
+	for (i= 0; i < 10; i++)
+	{
+	_putchar (letter [i]);
+	}
+	_putchar ('\n');
+	return (0);
+}
