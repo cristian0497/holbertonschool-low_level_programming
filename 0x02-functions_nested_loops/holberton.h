842374239318c0c31e11ffd9d8bf76cf4771f032
cristian0497 <cristian04.97@hotmail.com>
2019-09-24 14:25:16 +0000

    Header

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+
+void print_alphabet_x10(void);
+void print_alphabet(void);
+
+#endif
