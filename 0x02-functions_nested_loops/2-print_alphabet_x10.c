49b508695e844bd7f7b4cdea6e8a5d78260f279a
cristian0497 <cristian04.97@hotmail.com>
2019-09-24 14:37:53 +0000

    Correction with betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,23 @@
 #include "holberton.h"
 
 /**
- *main - alphabeth x10
+ * print_alphabet_x10 - alphabeth x10
  *
- *Return
+ * Return
  */
 
 void print_alphabet_x10(void)
 {
+
 	int x, count;
+
 	count = 1;
+
 	while (count <= 10)
 	{
 		for (x = 97; x <= 122; x++)
 		{
-			_putchar(x);
+			_putchar (x);
 		}
 		_putchar ('\n');
 		count++;
