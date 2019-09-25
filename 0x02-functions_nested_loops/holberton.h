33031ad4fd06bc476d5837e6519975aa8b6c3f3d
cristian0497 <cristian04.97@hotmail.com>
2019-09-25 01:06:52 +0000

    a+b

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,8 +7,9 @@ int print_sign(int n);
 int _abs(int a);
 int print_last_digit(int x);
 void jack_bauer(void);
-
+void times_table(void);
 void print_alphabet_x10(void);
 void print_alphabet(void);
+int add(int a, int b);
 
 #endif
