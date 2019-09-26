2549f6236d948011a7ccfca873c094b4fa65fe5b
cristian0497 <cristian04.97@hotmail.com>
2019-09-26 22:52:17 +0000

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_square - print diagonalprint square
+ * @size : var
+ * Return : No return
+ */						\
+
+void print_square(int size)
+{
+	int colum, pila;
+
+if (size > 0)
+{
+	for (colum = 0; colum < size; colum++)
+	{
+		for(pila = 0; pila < size; pila++)
+		{
+			_putchar(35);
+		}
+		_putchar ('\n');
+	}
+}
+else
+{
+	_putchar('\n');
+}
+}
