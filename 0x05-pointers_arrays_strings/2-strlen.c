70c96dc723930682b0ebbb03090e9c8023e694e8
cristian0497 <cristian04.97@hotmail.com>
2019-10-01 15:34:14 +0000

    Fix betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,18 +2,17 @@
 #include <string.h>
 
 /**
- *
- *
- *
+ * _strlen - Count char
+ * @s: funtion
+ * Return: 0 Succes
  */
 int _strlen(char *s)
 {
 	int contchar = 0;
 
-	while(s[contchar] != 0)
+	while (s[contchar] != 0)
 	{
 		contchar++;
 	}
 	return (contchar);
-
 }
