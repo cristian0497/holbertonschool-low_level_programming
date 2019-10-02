b344d6c0113e475c17bc0f90eeab0f603788df00
cristian0497 <cristian04.97@hotmail.com>
2019-10-02 05:28:34 +0000

    Fix with betty

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - array to show
+ * @a : Var1
+ * @n : size of array
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+
+	int cont = 0;
+
+	while (cont < x)
+	{
+			printf("%d", a[cont]);
+
+			if (cont != n - 1)
+			{
+				printf(", ");
+			}
+			cont++;
+	}
+	_putchar('\n');
+}
