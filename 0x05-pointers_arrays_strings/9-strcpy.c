ef95b7a0b13ac0411d49900378588fdd508e0b26
cristian0497 <cristian04.97@hotmail.com>
2019-10-02 20:07:56 +0000

    fix betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
+
 /**
- *
- *
- *
+ * *_strcpy - funtion copu
+ * @dest : var dest
+ * @src : var source
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int x;
 
-
 	for (x = 0; src[x] != '\0'; x++)
 	{
 		dest[x] = src[x];
 	}
 		dest[x] = '\0';
-
-	return dest;
+		return (dest);
 }
