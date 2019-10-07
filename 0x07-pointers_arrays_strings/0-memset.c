9f6fca5e3f4ce7ea39c33c158ae325a37dccb5c2
cristian0497 <cristian04.97@hotmail.com>
2019-10-07 11:07:49 -0500

    fix betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,21 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ ** _memset - variable to set
+ * @s : char to recieve
+ * @b : string to change
+ * @n : N times to add b in s
+ * Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
- unsigned int j;
+	unsigned int j;
 
-  for (j = 0; j < n; j++)
-    {
-      s[j] = b; 
-    }
-  return(s);
+	for (j = 0; j < n; j++)
+	{
+		s[j] = b;
+	}
+
+	return (s);
 }
