132a21a99305b5290291490f3844c3d11b5fc584
cristian0497 <cristian04.97@hotmail.com>
2019-10-07 10:53:12 -0500

    without betty

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+ unsigned int j;
+
+  for (j = 0; j < n; j++)
+    {
+      s[j] = b; 
+    }
+  return(s);
+}
