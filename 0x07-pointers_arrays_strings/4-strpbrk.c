4e2e9651474fb57da60603da5c37c9e96a5c379a
cristian0497 <cristian04.97@hotmail.com>
2019-10-07 17:56:53 -0500

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - Funtion to print
+ * @s: car type char
+ * @accept: funtion to compare
+ * Return: s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int x;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; accept[x] != '\0'; x++)
+		{
+			if (s[i] == accept[x])
+			{
+				s = (s + i);
+				goto salir;
+			}
+			if (s[i] == '\0')
+			{
+				s = "NULL";
+				break;
+			}
+		}
+	}
+salir: return (s);
+}
