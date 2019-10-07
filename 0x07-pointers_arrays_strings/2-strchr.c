ad5e3b647c4aaf14840678fa82ae39134b683fff
cristian0497 <cristian04.97@hotmail.com>
2019-10-07 12:33:11 -0500

    mem copy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - main to mem copy
+ * @s: string
+ * @c: char to find
+ * Return: null or s + i
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *a = "NULL";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (a);
+}
