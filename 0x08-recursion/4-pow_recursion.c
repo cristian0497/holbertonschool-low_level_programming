db2bc4eede5f6dd130cfb5f4b8ea2efa6805847c
cristian0497 <cristian04.97@hotmail.com>
2019-10-09 15:39:45 -0500

    Number to exp

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - funtion to exp
+ * @x : num
+ * @y : exp
+ * Return: x
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y == 1)
+		return (x);
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	x = x * _pow_recursion(x, y - 1);
+	return (x);
+}
