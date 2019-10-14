80c4e93c5db98a1acf4162e2d279fd593469263c
cristian0497 <cristian04.97@hotmail.com>
2019-10-13 19:12:40 -0500

    prin ARGC

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,16 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- *
- *
+ * main - print the n positions
+ * @argc: cont command line
+ * @argv: command line arguments
+ * Return: 0 succes
  */
 
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc);
+	return (0);
+}
