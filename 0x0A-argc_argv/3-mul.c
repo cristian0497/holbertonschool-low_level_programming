4dd4798a1f9bb1ee8ed701a7675313ec794c4181
cristian0497 <cristian04.97@hotmail.com>
2019-10-13 20:09:06 -0500

    fix return Error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,11 @@ int main(int argc, char *argv[])
 {
 	int x, y;
 
-	(void)argc;
+	if (argc < 3)
+	{
+	        printf("Error\n");
+		return (1);
+	}
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
 	printf("%d\n", (x * y));
