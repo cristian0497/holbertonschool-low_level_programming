5326da5e0835df6dac0abaface111d95a22f4405
cristian0497 <cristian04.97@hotmail.com>
2019-10-15 12:44:55 -0500

    Fix betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,17 +1,23 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * *create_array - funtion create array
+ * @size: var int
+ * @c: char
+ * Return: exit(assign)  fail(0);
+ */
+
 char *create_array(unsigned int size, char c)
 {
 	char *assign;
 	unsigned int cont;
 
 	assign = malloc(sizeof(char) * size);
-	for(cont = 0; cont < size; cont++)
+	for (cont = 0; cont < size; cont++)
 		assign[cont] = c;
 	assign[cont] = 0;
 	if (size < 1)
-		return(0);
-	else
-		return (assign);
+		return (0);
+	return (assign);
 }
