110bd04e741497b4ee6c0d03a661448233de8594
cristian0497 <cristian04.97@hotmail.com>
2019-10-16 09:12:56 -0500

    return null when assign malloc is null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int cont;
 
 	assign = malloc(sizeof(char) * size);
+	if (assign == NULL)
+		return(NULL);
 	for (cont = 0; cont <= size; cont++)
 		assign[cont] = c;
 	return (size <= 0 ? (NULL) : assign);
