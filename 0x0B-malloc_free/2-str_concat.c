f8a6432ac5c0c33ba8c34e64759971f0231ca9f2
cristian0497 <cristian04.97@hotmail.com>
2019-10-16 12:25:51 -0500

    Fix returns

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,18 +13,24 @@ char *str_concat(char *s1, char *s2)
 	char *string;
 	int x, y, z, m, n, o;
 
-	if (s1 == 0 || s2 == 0)
-		return (0);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (x = 0; s1[x] != '\0'; x++)
 		;
 	for (y = 0; s2[y] != '\0'; y++)
 		;
-	z = (x) + (y);
+	z = (x + y);
 	string = malloc(sizeof(char) * z);
-	for (m = 0; m < x; m++)
+	if (string == NULL)
+		return NULL;
+
+	for (m = 0; m <= x; m++)
 		string[m] = s1[m];
 	for (n = 0, o = x; n < y; n++, o++)
 		string[o] = s2[n];
-
+	string[o] = '\0';
 	return (string);
 }
