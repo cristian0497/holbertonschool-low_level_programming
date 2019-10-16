56abf30d3e49d734a53adfa97a09bbf58e410f41
cristian0497 <cristian04.97@hotmail.com>
2019-10-16 14:16:49 -0500

    fix betty

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,9 +3,10 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * free_grid - funtion freed space
+ * @grid: array to will free
+ * @height: size of array
+ * Retur: void
  */
 void free_grid(int **grid, int height)
 {
