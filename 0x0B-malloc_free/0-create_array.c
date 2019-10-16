8a11b58333d9cd80dc142b1ca442dd52401fdf58
cristian0497 <cristian04.97@hotmail.com>
2019-10-16 09:01:15 -0500

    fail return malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,8 @@ char *create_array(unsigned int size, char c)
 	char *assign;
 	unsigned int cont;
 
-	assign =  malloc(sizeof(char) * size);
-	for (cont = 0; cont < size; cont++)
+	assign = malloc(sizeof(char) * size);
+	for (cont = 0; cont <= size; cont++)
 		assign[cont] = c;
 	return (size <= 0 ? (NULL) : assign);
 }
