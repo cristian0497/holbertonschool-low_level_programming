ce472a275dba80769200df46d370991638124152
cristian0497 <cristian04.97@hotmail.com>
2019-10-16 11:05:52 -0500

    Fix return values

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,16 +12,16 @@ char *_strdup(char *str)
 	char *text;
 	int i, j;
 
-	while (str[i] != '\0')
-		i++;
-	i++;
-
+	for (i = 0; str[i] != '\0'; i++)
+		;
 	text = malloc(sizeof(char) * i);
 	if (text == NULL)
+	{
+		printf("text nulo");
 		return (NULL);
-
+	}
 	for (j = 0; j < i; j++)
 		text[j] = str[j];
-
+	text[j] = '\0';
 	return (text);
 }
