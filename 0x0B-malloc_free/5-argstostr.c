08af63c796ec2f3e71a076c4d403546588758545
cristian0497 <cristian04.97@hotmail.com>
2019-10-16 15:40:00 -0500

    Cont arguments

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - funtion string leng
+ * @str: string
+ * Return: len
+ */
+
+int _strlen(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] == '\0'; x++)
+		;
+	return (x);
+}
+
+/**
+ * *argstostr - funtion concatenate arg
+ * @ac: size of arguments
+ * @av: arguments
+ * Return: arguments concatenated
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *string;
+	int x, sum;
+	int m, n;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	sum = 0;
+	for (x = 0; x < ac; x++)
+		{
+			sum += _strlen(av[x]);
+		}
+	string = malloc((sum + ac + 1) * sizeof(char));
+	if (string == NULL)
+	{
+		free(string);
+		return (NULL);
+	}
+	for (m = 0, sum = 0; m < ac; m++)
+	{
+		for (n = 0; av[m][n] != '\0'; n++, sum++)
+		{
+			string[sum] = av[m][n];
+		}
+		string[sum] = '\n';
+		sum++;
+	}
+	return (string);
+}
