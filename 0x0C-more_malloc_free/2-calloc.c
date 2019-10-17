24adff2fbf53c558a5b6c235e52e39f567647814
cristian0497 <cristian04.97@hotmail.com>
2019-10-17 15:49:59 -0500

    return to char

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
  * _calloc - funtion alocate malloc
  * @nmeb: size
@@ -10,17 +9,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *string;
+	char *string;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	string = malloc((size * 4) * nmemb);
+	string = malloc(nmemb + size);
 
 	if (string == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
-		string[i] = '0';
+		string[i] = 0;
 	return ((void *)string);
 }
