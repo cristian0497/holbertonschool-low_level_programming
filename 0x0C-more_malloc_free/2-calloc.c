9acc5b81dce3c8358075ec74a725acf0c0d08cb4
cristian0497 <cristian04.97@hotmail.com>
2019-10-17 14:21:26 -0500

    change '0' for 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,8 +16,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	string = malloc(sizeof(int) * nmemb);
 	for (i = 0; i < nmemb; i++)
-	{
-		string[i] = '0';
-	}
+		string[i] = 0;
 return (string);
 }
