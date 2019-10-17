8322a46e3c463f90e5578ce6fce12b319dc33e1e
cristian0497 <cristian04.97@hotmail.com>
2019-10-17 14:09:32 -0500

    Fails con valgrind

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - funtion alocate malloc
+ * @nmeb: size
+ * @size: size of pointer
+ * Return: string
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *string;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	string = malloc(sizeof(int) * nmemb);
+	for (i = 0; i < size; i++)
+	{
+		string[i] = '0';
+	}
+return (string);
+}
