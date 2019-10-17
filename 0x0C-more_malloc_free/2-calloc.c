ccc2730d294f76de5a1c977c0fa4470e204807a3
cristian0497 <cristian04.97@hotmail.com>
2019-10-17 16:10:20 -0500

    last time for today

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,11 +15,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
-	string = malloc(nmemb + size);
+	string = malloc((size * nmemb) + 1);
 
 	if (string == NULL)
 		return (NULL);
+
 	for (i = 0; i < nmemb; i++)
 		string[i] = 0;
+	string[i] = '\0';
 	return ((void *)string);
 }
