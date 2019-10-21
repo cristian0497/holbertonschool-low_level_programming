73091f6cf8323c05d553e9868db01e222985e187
cristian0497 <cristian04.97@hotmail.com>
2019-10-21 15:00:20 -0500

    Change (*d).name by d -> name

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 
 /**
  * init_dog - funtion to assign to struct
@@ -11,7 +10,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
