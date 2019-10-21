cf2b91ee767f9a9d5301f3b20c419c4b4af26d33
cristian0497 <cristian04.97@hotmail.com>
2019-10-21 16:00:07 -0500

    firt up

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - funtion print data
+ * @d: data structure
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
