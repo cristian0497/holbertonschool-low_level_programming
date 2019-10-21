d97a1ea399a2348a28432cd082485c12fe689a4e
cristian0497 <cristian04.97@hotmail.com>
2019-10-21 14:53:42 -0500

    inizialize type

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - funtion to assign to struct
+ * @d: struct type
+ * @name: 1 var
+ * @age: age of dog
+ * @owner: owner od the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
