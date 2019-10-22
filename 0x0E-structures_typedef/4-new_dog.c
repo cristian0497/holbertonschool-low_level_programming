3bcb11b48398bbb57328d082d7f69e1f9e355038
cristian0497 <cristian04.97@hotmail.com>
2019-10-21 22:33:40 -0500

    add free funtion

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,20 +53,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	ret = malloc(sizeof(struct dog));
 	if (ret == NULL)
+	{
+		free(ret);
 		return (NULL);
-
+	}
 	x = _strlen(name);
 	ret->name = malloc(sizeof(char) * (x + 1));
 	if (ret->name == NULL)
+	{
+		free(ret->name);
 		return (NULL);
+	}
 	_strcpy(ret->name, name);
 
 	ret->age = age;
+	if (ret->age <= 0)
+		return (0);
 
 	y = _strlen(owner);
 	ret->owner = malloc(sizeof(char) * (y + 1));
 	if (ret->owner == NULL)
+	{
+		free(ret->owner);
 		return (NULL);
+	}
 	_strcpy(ret->owner, owner);
 
 	return (ret);
