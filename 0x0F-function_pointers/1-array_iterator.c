0487b4fe574a501574383cbf199426626384b1be
cristian0497 <cristian04.97@hotmail.com>
2019-10-23 10:47:15 -0500

    [Fix] Betty -  comments

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 
 /**
- *
- *
+ * array_iterator - function run a function
+ * @array: array with the numbers to print
+ * @size: size of array
+ * @action: pointer to addres of a funtion
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
