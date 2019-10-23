062345dbb6e4997318fa9717f8c9b0aecebc3cb9
cristian0497 <cristian04.97@hotmail.com>
2019-10-23 10:28:35 -0500

    [Feature] invoque the funtion to print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - funtion join funtions
+ * @name: name to printf
+ * @f: pointer to funtion with set char
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
