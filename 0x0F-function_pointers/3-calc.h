4c37e8c80511bc5a6c49b24bd99afce3c5017c44
cristian0497 <cristian04.97@hotmail.com>
2019-10-23 22:47:02 -0500

    [Fix] add libraries to any file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,8 @@
 #ifndef _3_CALC_H_
 #define _3_calc_H_
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct op - Struct op
  *
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,7 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * get_op_func - pointer to funtion
  * @s: operator passed argument program
