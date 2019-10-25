d935a0d934121a3630d8fa791e839169c7356be5
cristian0497 <cristian04.97@hotmail.com>
2019-10-25 12:27:18 -0500

    [feature] add file 1-print_numbers.c [Feature] add prototype P1 to header

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - funtion to print arguments
+ * @separator: String separator
+ * @n: number of elements
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+	va_list mylist;
+	unsigned int cont;
+	if (separator)
+	{
+	va_start(mylist, n);
+
+	for(cont = 0; cont < n; cont++)
+	{
+		printf("%d", va_arg(mylist, int));
+		if (cont < n - 1)
+			printf("%s", separator);
+	}
+	va_end(mylist);
+	if (cont == n)
+		printf("\n");
+	}
+}
