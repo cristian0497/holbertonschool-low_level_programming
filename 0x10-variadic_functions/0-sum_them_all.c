824f22970b5726bb9b5ef04ba20b664e7058cab3
cristian0497 <cristian04.97@hotmail.com>
2019-10-26 12:49:50 -0500

    [Fix] problem with betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,9 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- *
- *
+ * sum_them_all - funtion sum args
+ * @n: args to sum
+ * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -12,7 +13,6 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	else
 	va_start(mylist, n);
 
 	for (cont = 0; cont < n; cont++)
