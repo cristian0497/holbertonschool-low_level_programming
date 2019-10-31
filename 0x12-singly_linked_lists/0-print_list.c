76a446baf9a2c520853b86c0e3a962a17f061109
cristian0497 <cristian04.97@hotmail.com>
2019-10-31 11:01:34 -0500

    [Feature] add the funtion

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - funtion print linked list
+ * @h: const type pointer list_t
+ * Return: cont times
+ */
+
+size_t print_list(const list_t *h)
+{
+	int cont = 0;
+
+	while (h != NULL)
+	{
+		if (!h->str)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+			h = h->next;
+			cont++;
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		cont++;
+	}
+	return (cont);
+}
