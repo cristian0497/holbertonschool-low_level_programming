e05c2fa707f99b4dfc4b3c3a0d5c2112e8d36e14
cristian0497 <cristian04.97@hotmail.com>
2019-10-31 19:50:32 -0500

    Fix, betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 /**
- *
- *
+ * before_main_funtion - funtion that work before of main
  */
-void before_main(void)
+void before_main_funtion(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,5 +27,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-void before_main(void)__attribute__((constructor));
+void before_main_funtion(void)__attribute__((constructor));
 #endif
