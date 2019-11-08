28bed97e2989c28886d88581086f288e32462f41
cristian0497 <cristian04.97@hotmail.com>
2019-11-07 19:12:15 -0500

    [Add] add new file "2-get_bit.c", betty OK [Feature] add protptype of task 2. betty OK

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * get_bit - funtion that obtain nth bit
+ * @n: numbert dec to convert in bin
+ * @index: index to search
+ * Return: 1 -0 or -1 it fail
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int cont;
+	unsigned long int tmp = n;
+
+	cont = 0;
+	while ((tmp >>= 1) > 0)
+		cont++;
+	while (cont >= 0)
+	{
+		if ((n >> cont--) & 1)
+		{
+			if (cont + 1 == (int)index)
+				return (1);
+		}
+		else
+		{
+			if (cont + 1 == (int)index)
+				return (0);
+		}
+	}
+	return (-1);
+}
