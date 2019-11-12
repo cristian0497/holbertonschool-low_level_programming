fdf1a5a9142c98cc0f30314f88a1da54590c713b
cristian0497 <cristian04.97@hotmail.com>
2019-11-12 12:53:23 -0500

    Fix, return fail codes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,27 +25,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 	int r;
 
-	ret_val = open(filename, O_RDONLY);
-	if (ret_val == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	r = read(ret_val, buf, letters);
-	if (!r)
-		return (0);
-
-	ret_val = _strlen(buf);
-	ret_val = write(STDOUT_FILENO, buf, ret_val);
-	if (ret_val == -1)
+	if (filename)
 	{
+		ret_val = open(filename, O_RDONLY);
+		if (ret_val == -1)
+			return (0);
+
+		buf = malloc(sizeof(char) * letters);
+		if (!buf)
+			free(buf);
+			return (0);
+		r = read(ret_val, buf, letters);
+		if (r == -1)
+			return (0);
+
+		ret_val = _strlen(buf);
+		ret_val = write(STDOUT_FILENO, buf, ret_val);
+		if (ret_val == -1)
+		{
+			free(buf);
+			return (0);
+		}
 		free(buf);
-		return (0);
+		close(ret_val);
+		return (ret_val);
 	}
-
-	free(buf);
-	close(ret_val);
-	return (ret_val);
-
 }
