56d133be7fbcaeec16ecb5af068fd10911513cf7
cristian0497 <cristian04.97@hotmail.com>
2019-11-12 14:41:40 -0500

    Add, new files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * _strlen - funtion that count num of chars
+ * @s: string to count
+ * Return: lengh of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * create_file - funtion that create a file
+ * @filename: name of the file to create
+ * @text_content: content to add at file
+ * Return: 1 Succes, -1 error.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int ret_val, len;
+
+	if (filename == NULL)
+		return (-1);
+	ret_val = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (ret_val == -1)
+		return (-1); /* Verificar retorno error */
+	len = _strlen(text_content);
+	write(ret_val, text_content, len);
+	close(ret_val);
+	return (1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -6,8 +6,9 @@
 #include <fcntl.h> /* OPEN */
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> /* WRITE */
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
