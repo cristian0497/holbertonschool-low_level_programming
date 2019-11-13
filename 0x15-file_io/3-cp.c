9a7d81520b9f7d66b3a2c5261130353067269948
cristian0497 <cristian04.97@hotmail.com>
2019-11-13 10:22:26 -0500

    Fix, check #5

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,12 +15,12 @@
 int main(int ac, char **av)
 {
 	ssize_t rd;
-	int  ret_val, wr, ret_val2;
+	int  ret_val, ret_val2;
 	char  buf[BUFFER_SIZE], *file_from, *file_to;
 
 	file_from = av[1], file_to = av[2];
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "usage cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	ret_val = open(file_from, O_RDONLY);
 	if (ret_val == -1)
 	{
@@ -32,8 +32,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
 	while ((rd = read(ret_val, buf, BUFFER_SIZE)) > 0)
 	{
-		wr = write(ret_val2, buf, rd);
-		if (wr != rd)
+		if (write(ret_val2, buf, rd) != rd)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
 	}
 	if (rd == -1)
