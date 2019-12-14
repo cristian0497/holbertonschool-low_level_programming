85ee99305f37b62e5a4c09c3c6fa422329b40e2a
cristian0497 <cristian04.97@hotmail.com>
2019-12-13 20:52:00 -0500

    [ADD] new prototype

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -18,7 +20,8 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h); /* 0 */
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif
