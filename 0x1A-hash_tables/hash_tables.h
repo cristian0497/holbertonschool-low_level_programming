b5eff4fba2280e87387de16d4a77a243963184c0
cristian0497 <cristian04.97@hotmail.com>
2020-01-18 08:34:42 -0500

    [FIX] Add stddef.h and stdlib.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,7 @@
 #ifndef _HASH_TABLES_H_
 #define _HASH_TABLES_H_
+#include <stddef.h> /* NULL value */
+#include <stdlib.h> /* Malloc and Calloc */
 /**
  * struct hash_node_s - Node of a hash table
  *
