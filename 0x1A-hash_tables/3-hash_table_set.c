2352e041f58f1562e7722248af1101e175229f87
cristian0497 <cristian04.97@hotmail.com>
2020-01-19 16:01:20 -0500

    [FIX] if conditional statement

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (strlen(key) == 0)
 		return (0);
-	index = key_index((unsigned char *)node->key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index])
 	{
 		tmp = ht->array[index];
