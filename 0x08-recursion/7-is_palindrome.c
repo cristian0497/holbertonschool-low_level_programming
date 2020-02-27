23cd1ab14a3deede22e4164f8893ca325564e1c2
cristian0497 <cristian04.97@hotmail.com>
2020-02-26 20:23:00 -0500

    [FIX] time ago..........

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,26 +1,46 @@
 #include "holberton.h"
-
-
-int long_s (char *string_cont)
-{
-	if (*string_cont == '\0')
-		return (0);
-	return (long_s(++string_cont + 1));
-}
-int retorno (char *s, int cont_del, int cont_rev)
-{
-	if ( s[cont_del] != s[cont_rev])
-		return (0);
-	else
-		return (retorno (s, cont_del + 1, cont_rev - 1));
-}
-
+/**
+ * is_palindrome - define two idx
+ * @s: chars to compare
+ * Return: 1 for palindrome, 0 if no
+ */
 int is_palindrome(char *s)
 {
-	int large;
-	large = long_s(s);
+	int len = _strlen_recursion(s);
+	int x = 0;
 
-	if (large <= 1)
-		return(0);
-	return (retorno(s, 0, large));
+	if (len == 0)
+		return (1);
+	else
+		return (recursion(s, x, len - 1));
+}
+/**
+ * _strlen_recursion - define the len of the string
+ * @s: string to compare
+ * Return: len of string
+ */
+int _strlen_recursion(char *s)
+{
+	int x = -1;
+
+	if (*s != '\0')
+		x = _strlen_recursion(s + 1);
+	return (x + 1);
+}
+/**
+ * recursion - define if a string is a palindrome or no
+ * @s: string
+ * @x: idx left
+ * @len: idx right
+ * Return: 1 for palindrome or 0 if not
+ */
+int recursion(char *s, int x, int len)
+{
+	int ret = 0;
+
+	if (x >=  len)
+		return (1);
+	if (s[x] == s[len])
+		ret = recursion(s, x + 1, len - 1);
+	return (ret);
 }
