97ae413edf00db34a1018cbea408c749d2fd9ed1
cristian0497 <cristian04.97@hotmail.com>
2020-03-21 10:20:56 -0500

    [FIX] Add new conditionsl

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * times_table - sum
- *
+ * print_times_table - sum
+ * @n: can of prints
  * Return: 0 (Succes)
  */
 void print_times_table(int n)
@@ -9,35 +9,27 @@ void print_times_table(int n)
 	int contver, conthor;
 	int numero, centena, decena, unidad, res;
 
+	if (n < 0 || n > 15)
+		return;
 	if (n > 0 && n < 15)
 	{
 		for (contver = 0; contver <= n; contver++)
 		{
 			for (conthor = 0; conthor <= n; conthor++)
 			{
-				numero = (contver * conthor);
-				centena = numero / 100;
-				res = numero % 100;
-				decena = res / 10;
+				numero = (contver * conthor), centena = numero / 100;
+				res = numero % 100, decena = res / 10;
 				unidad = res % 10;
 				if (conthor != 0)
 				{
 					if (centena == 0)
-					{
 						_putchar (32);
-					}
 					else
-					{
 						_putchar (centena + '0');
-					}
 					if ((decena == 0) && (centena != 1))
-					{
 						_putchar (32);
-					}
 					else
-					{
-					_putchar (decena + '0');
-					}
+						_putchar (decena + '0');
 				}
 					_putchar (unidad + '0');
 				if (conthor < n)
@@ -46,15 +38,11 @@ void print_times_table(int n)
 					_putchar (32);
 				}
 				else if (conthor > n)
-				{
 					conthor = 0;
-				}
 			}
 			_putchar ('\n');
 		}
 	}
 	else if (n <= 0 || n >= 15)
-	{
 		_putchar('\n');
-	}
 }
